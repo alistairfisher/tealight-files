5df41ba3ca13aa25d88797d92e1f131642f1ef8d
alistairfisher <alistairstfisher@gmail.com>
2015-08-18 10:35:03 +0100

    Closing logo/polygon.py

--- logo/polygon.py
+++ logo/polygon.py
@@ -1,9 +1,1 @@
-from tealight.logo import move, turn
-
-def polygon(999, size):
-  angle = 360.0 / edges
-  for i in range(0, edges):
-    move(size)
-    turn(angle)
-    
-polygon(6,150)
+Loading logo/polygon.py...
